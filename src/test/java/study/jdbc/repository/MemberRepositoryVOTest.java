95ef87ed2a4f2d2dc8aaaf251e2c5d2def4e56dd
hoho <ghdnjs18@naver.com>
2024-04-19 10:32:49 +0900

    JDBC 수정, 삭제 구현

--- src/test/java/study/jdbc/repository/MemberRepositoryVOTest.java
+++ src/test/java/study/jdbc/repository/MemberRepositoryVOTest.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import study.jdbc.domain.Member;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -14,12 +16,22 @@ class MemberRepositoryVOTest {
     @Test
     public void crud() throws Exception {
         // save
-        Member member = new Member("memberV1", 10000);
+        Member member = new Member("memberV2", 10000);
         repository.save(member);
 
         // findById
         Member findMember = repository.findById(member.getMemberId());
         log.info("findmember = {}", findMember);
         assertEquals(findMember, member);
+
+        // update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        log.info("updateMember = {}", updateMember);
+        assertEquals(updateMember.getMoney(), 20000);
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThrows(NoSuchElementException.class, () -> repository.findById(member.getMemberId()));
     }
 }
